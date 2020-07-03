d5548373c0740bf1a91b9463c073f86cbde0e00b
kamchur <juhwan5147@naver.com>
2020-07-03 09:58:14 +0900

    construct : ProgressBar operate Motion

---
+++ app/src/main/java/com/kamchur/lotto/Load.java
@@ -0,0 +1,42 @@
+package com.kamchur.lotto;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+
+public class Load extends Dialog {
+    public Load(@NonNull Context context) {
+        super(context);
+    }
+
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        setContentView(R.layout.progressbar);
+        setCancelable(false);
+        setCanceledOnTouchOutside(false);
+
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.progress_anim);
+        anim.setDuration(3000);
+        progressBar.startAnimation(anim);
+
+    }
+
+
+
+}
