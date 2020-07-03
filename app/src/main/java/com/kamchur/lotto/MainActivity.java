6e30d79925ba3f3f12ce4d9b40d4e5fe7d853ee1
kjh0818 <kjh0818@tjmedia.com>
2020-07-03 09:38:48 +0900

    construct Button View structure ++

--- app/src/main/java/com/kamchur/lotto/MainActivity.java
+++ app/src/main/java/com/kamchur/lotto/MainActivity.java
@@ -3,12 +3,35 @@ package com.kamchur.lotto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button luckyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO : 버튼 누름시 약 3초간 동그란 프로그레스바 동작
+
+        luckyButton = (Button) findViewById(R.id.luckyButton);
+
+        luckyButton.setOnClickListener(clickListener);
+
     }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            switch (view.getId()){
+                case R.id.luckyButton:
+                    
+
+            }
+        }
+    };
 }
