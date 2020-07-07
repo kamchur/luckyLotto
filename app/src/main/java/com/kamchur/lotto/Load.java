d1d3f8e671ed2e05c63cc9e08112578a5668486b
kamchur <juhwan5147@naver.com>
2020-07-07 17:15:29 +0900

    tttttt

--- app/src/main/java/com/kamchur/lotto/Load.java
+++ app/src/main/java/com/kamchur/lotto/Load.java
@@ -19,6 +19,8 @@ public class Load extends Dialog {
 
     private ProgressBar progressBar;
 
+    private int dddd= 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
